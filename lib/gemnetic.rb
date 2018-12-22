d618cad4fc7b1e7cba24d91dba8a066b3629cfbc
Canedo <canedo@webgoal.com.br>
2018-12-22 18:19:16 -0200

    gem structure

---
+++ lib/gemnetic.rb
@@ -0,0 +1,5 @@
+require "gemnetic/version"
+
+module Gemnetic
+  # Your code goes here...
+end
---
+++ spec/gemnetic_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Gemnetic do
+  it "has a version number" do
+    expect(Gemnetic::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
