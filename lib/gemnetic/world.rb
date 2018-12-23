ae24a06a2e77d31887a1b4d21e0802bffa1d7cab
Canedo <canedo@webgoal.com.br>
2018-12-23 16:00:35 -0200

    refactoring the world initializer

--- lib/gemnetic/world.rb
+++ lib/gemnetic/world.rb
@@ -1,17 +1,19 @@
+require 'pickup'
+
 module Gemnetic
   class World
     attr_accessor :population_size, :population_keep, :population
     attr_reader :specimen_class, :current_generation
 
     def initialize(specimen_class, params = {})
-      raise ArgumentError.new("Specimen Class can't be nil") unless specimen_class
-      raise ArgumentError.new("Specimen Class must be a kind of Gemnetic::Specimen") unless specimen_class.ancestors.include?(Gemnetic::Specimen)
-
       @specimen_class = specimen_class
       @population_size = params.fetch(:population_size, 50)
       @population_keep = params.fetch(:population_keep, 5)
+
       @current_generation = 0
       @population = []
+
+      validate
 
       if params.fetch(:initialize_population, true)
         @population_size.times do
@@ -28,6 +30,13 @@ module Gemnetic
 
     def population_to_keep
       @population.sort_by { |specimen| specimen.evaluate }.last(@population_keep)
+    end
+
+    private
+
+    def validate
+      raise ArgumentError.new("Specimen Class can't be nil") unless @specimen_class
+      raise ArgumentError.new("Specimen Class must be a kind of Gemnetic::Specimen") unless @specimen_class.ancestors.include?(Gemnetic::Specimen)
     end
 
   end
