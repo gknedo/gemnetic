0ea651ff8765a5263f5401b781c6b22174634b65
Canedo <canedo@webgoal.com.br>
2018-12-23 22:28:27 -0200

    rewrite of world initialization

--- lib/gemnetic/world.rb
+++ lib/gemnetic/world.rb
@@ -8,20 +8,19 @@ module Gemnetic
     def initialize(specimen_class, params = {})
       @specimen_class = specimen_class
       @population_size = params.fetch(:population_size, 50)
-      @population_keep = params.fetch(:population_keep, 5)
+      @population_keep = params.fetch(:population_keep, @population_size/10)
+      @pickup = Pickup.new({
+        mix: params.fetch(:mix_probability, 4),
+        mutate: params.fetch(:mutate_probability, 1),
+        tournament: params.fetch(:tournament_probability, 5)
+      })
 
       @current_generation = 0
       @population = []
 
       validate
 
-      if params.fetch(:initialize_population, true)
-        @population_size.times do
-          specimen = @specimen_class.new
-          specimen.generation = @current_generation
-          @population.push specimen
-        end
-      end
+      initialize_population if params.fetch(:initialize_population, true)
     end
 
     def next_generation
@@ -37,6 +36,14 @@ module Gemnetic
     def validate
       raise ArgumentError.new("Specimen Class can't be nil") unless @specimen_class
       raise ArgumentError.new("Specimen Class must be a kind of Gemnetic::Specimen") unless @specimen_class.ancestors.include?(Gemnetic::Specimen)
+    end
+
+    def initialize_population
+      @population_size.times do
+        specimen = @specimen_class.new
+        specimen.generation = @current_generation
+        @population.push specimen
+      end
     end
 
   end
